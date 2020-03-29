3758ed06a55e01af0fc36f4929852f195bab5324
Patrick Cate <patrickcate@users.noreply.github.com>
2020-03-29 16:55:08 -0400

    fix: fix createWrapper() not merging state together

--- test/unit/setup.js
+++ test/unit/setup.js
@@ -1,6 +1,5 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
 import Vuex from 'vuex'
+import VueMeta from 'vue-meta'
 import { shallowMount, mount, createLocalVue } from '@vue/test-utils'
 import mockData from '@fixtures/mock-data.js'
 import {
@@ -10,6 +9,24 @@ import {
   actions as defaultActions,
 } from '~/store/index.js'
 
+// Make console.error throw, so that Jest tests fail
+const error = console.error
+console.error = function(message) {
+  error.apply(console, arguments)
+  // NOTE: You can whitelist some `console.error` messages here
+  //       by returning if the `message` value is acceptable.
+  throw message instanceof Error ? message : new Error(message)
+}
+
+// Make console.warn throw, so that Jest tests fail
+const warn = console.warn
+console.warn = function(message) {
+  warn.apply(console, arguments)
+  // NOTE: You can whitelist some `console.warn` messages here
+  //       by returning if the `message` value is acceptable.
+  throw message instanceof Error ? message : new Error(message)
+}
+
 const createStore = ({
   state = {},
   getters = {},
@@ -18,7 +35,7 @@ const createStore = ({
 }) =>
   new Vuex.Store({
     state: {
-      ...defaultState,
+      ...defaultState(),
       ...mockData.store.state,
       ...state,
     },
@@ -37,6 +54,8 @@ const createStore = ({
     },
   })
 
+const pageComponents = ['HomePage', 'ArtworkPage']
+
 global.createWrapper = (
   component,
   options = {},
@@ -44,15 +63,23 @@ global.createWrapper = (
   mountType = 'shallow'
 ) => {
   const localVue = createLocalVue()
-  localVue.use(VueRouter)
+
   localVue.use(Vuex)
 
+  if (pageComponents.includes(component.name)) {
+    localVue.use(VueMeta, { keyName: 'head' })
+  }
+
   const store = createStore(storeState)
-  const router = new VueRouter()
 
   const RootComponent = {
     name: 'RootComponent',
     template: '<div><slot></slot></div>',
+    data() {
+      return {
+        swipers: {},
+      }
+    },
   }
 
   let wrapper
@@ -61,7 +88,6 @@ global.createWrapper = (
     wrapper = mount(component, {
       parentComponent: RootComponent,
       store,
-      router,
       localVue,
       stubs: {
         ...mockData.stubs,
@@ -72,17 +98,12 @@ global.createWrapper = (
     wrapper = shallowMount(component, {
       parentComponent: RootComponent,
       store,
-      router,
       localVue,
       stubs: {
         ...mockData.stubs,
       },
       ...options,
     })
-  }
-
-  wrapper.vm.$root = {
-    swipers: {},
   }
 
   return wrapper
