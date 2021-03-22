e56dcc99dfc0c97bbfca22b7835663fabbb77f59
Patrick Cate <patrickcate@users.noreply.github.com>
2021-03-21 23:19:18 -0400

    fix: fix failing cypress test

--- tests/e2e/specs/artwork-slide.spec.js
+++ tests/e2e/specs/artwork-slide.spec.js
@@ -1,81 +1,66 @@
 describe('artwork slide', () => {
+  const carouselSelector = '.l-page__carousel'
+
   beforeEach(() => {
     cy.viewport('iphone-6')
     cy.visit('/01-01')
   })
 
   it('carousel height updates when active artwork height changes', () => {
-    const carouselSelector = '.l-page__carousel'
+    cy.get(carouselSelector).as('pageCarousel').should('be.visible')
 
-    cy.get(carouselSelector)
-      .should('be.visible')
-      .then(() => {
-        let currentHeight = cy.$$(carouselSelector)[0].style.height
+    let currentHeight = cy.$$(carouselSelector)[0].style.height
 
-        for (let step = 0; step < 6; step++) {
-          cy.get('.timeline-nav-button--prev').click()
+    for (let step = 0; step < 6; step++) {
+      cy.get('.timeline-nav-button--prev').click()
 
-          cy.wait(1000).then(() => {
-            const newHeight = cy.$$(carouselSelector)[0].style.height
+      cy.get('@pageCarousel')
+        .should('be.visible')
+        .should('not.have.css', 'height', `${currentHeight}px`)
 
-            expect(newHeight).to.not.eq(currentHeight)
-
-            currentHeight = cy.$$(carouselSelector)[0].style.height
-          })
-        }
-      })
+      currentHeight = cy.$$(carouselSelector)[0].style.height
+    }
   })
 
   it('carousel height is updated when screen is resized', () => {
-    const carouselSelector = '.l-page__carousel'
+    cy.get(carouselSelector).as('pageCarousel').should('be.visible')
 
-    cy.get(carouselSelector)
+    const currentHeight = cy.$$(carouselSelector)[0].style.height
+
+    cy.viewport('macbook-15')
+
+    cy.get('@pageCarousel')
       .should('be.visible')
-      .then(() => {
-        const currentHeight = cy.$$(carouselSelector)[0].style.height
-
-        cy.viewport('macbook-15')
-
-        cy.wait(1000).then(() => {
-          const newHeight = cy.$$(carouselSelector)[0].style.height
-
-          expect(newHeight).to.not.eq(currentHeight)
-        })
-      })
+      .should('not.have.css', 'height', `${currentHeight}px`)
   })
 
   it('swipe left gesture changes active slide', () => {
-    cy.get('.timeline-nav__list-item.swiper-slide-active').should(
-      'contain',
-      '01/01'
-    )
+    const activeTimelineSlideSelector =
+      '.timeline-nav__list-item.swiper-slide-active'
 
-    cy.wait(1000)
+    cy.get(activeTimelineSlideSelector).should('contain', '01/01')
 
     // Simulate swiper right gesture.
     cy.get('.l-page__carousel')
+      .as('pageCarousel')
       .should('be.visible')
-      .trigger('pointerdown', { which: 1 })
-      .trigger('pointermove', 'right')
+      .trigger('pointerdown', { which: 1, force: true })
+      .trigger('pointermove', 'right', { force: true })
       .trigger('pointerup', { force: true })
 
-    cy.get('.timeline-nav__list-item.swiper-slide-active').should(
-      'contain',
-      '12/31'
-    )
-
-    cy.wait(1000)
+    cy.get(activeTimelineSlideSelector)
+      .not('.swiper-slide-next')
+      .should('contain', '12/31', {
+        timeout: 4000,
+      })
 
     // Simulate swiper left gesture.
-    cy.get('.l-page__carousel')
+    cy.get('@pageCarousel')
       .should('be.visible')
-      .trigger('pointerdown', { which: 1 })
-      .trigger('pointermove', 'left')
+      .trigger('pointerdown', { which: 1, force: true })
+      .trigger('pointermove', 'left', { force: true })
       .trigger('pointerup', { force: true })
 
-    cy.get('.timeline-nav__list-item.swiper-slide-active').should(
-      'contain',
-      '01/01'
-    )
+    cy.get(activeTimelineSlideSelector).should('contain', '01/01')
   })
 })
